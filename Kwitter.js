70a3930d54f5a16715839c09dfe1133feb2f3e24
pedroithiel <pedro.ithiel@gmail.com>
2022-09-08 18:40:27 -0300

    Update Kwitter.js

--- Kwitter.js
+++ Kwitter.js
@@ -1,5 +1,5 @@
 function logar() {
     var login = document.getElementById("nomeUsuario").value;
     localStorage.setItem("nome", login);
-    window.location= "kwitter_room.html";
+    window.location= "Kwitter_room.html";
 }
